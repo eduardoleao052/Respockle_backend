b0e6996b7a4872b5bdf640c729fc7ab5d0b917a6
Terciotti <pedro.terciotti@usp.br>
2024-12-01 22:07:58 -0300

    Add CustomUser to admin

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Community, Comment, Profile
+from .models import Post, Community, Comment, Profile, User, CustomUserAdmin
 
 
 # Register your models here.
@@ -7,3 +7,5 @@ admin.site.register(Post)
 admin.site.register(Community)
 admin.site.register(Comment)
 admin.site.register(Profile)
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- backend/settings.py
+++ backend/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = "backend.CustomUserAdmin"
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
